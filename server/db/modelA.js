32fb004f1098df5eec862e43a873c562816b28b3
Lizgo14 <lizgordon14@gmail.com>
2019-12-26 14:59:52 -0500

    Authorization complete

--- server/db/modelA.js
+++ server/db/modelA.js
@@ -1,14 +1,53 @@
 const db = require('./db')
 const Sequelize = require('sequelize')
+const crypto = require('crypto')
 
 const ModelA = db.define('modelA',{
   username: {
     type: Sequelize.STRING,
-    allowNull: false
+    allowNull: false,
+    unique: true
   },
   password: {
+    type: Sequelize.STRING,
+  },
+  salt: {
     type: Sequelize.STRING
-  }
-})
+    }
+  }, {
+    hooks:{
+      beforeCreate: setSaltPassword,
+      beforeUpdate: setSaltPassword
+    }
+  })
 
+
+ModelA.prototype.correctPassword = function(password){
+  return this.Model.encryptPassword(password,this.salt) === this.password
+}
+
+User.prototype.sanitize = function () {
+  return _.omit(this.toJSON(), ['password', 'salt']);
+}
+
+
+ModelA.generateSalt = function() {
+  return crypto.randomBytes(16).toString('base64')
+}
+
+
+ModelA.encryptPassword = function (plainText, salt) {
+  const hash = crypto.createHash('sha1')
+  hash.update(plainText)
+  hash.update(salt)
+  return hash.digest
+}
+
+
+function setSaltAndPassword (user){
+  if (user.changed('password')){
+    user.salt= ModelA.generateSalt()
+    user.password = ModelA.encryptPassword(user.password, user.salt)
+  }
+}
 module.exports = ModelA
--- server/routes/modelARoutes.js
+++ server/routes/modelARoutes.js
@@ -10,4 +10,61 @@ router.get('', async(req,res,next) => {
   }
 })
 
+const userNotFound = next => {
+  const err = new Error('Not found')
+  err.status = 404
+  next(err)
+}
+
+router.get('/me', (req,res,next) => {
+  if (!req.session.userId){
+    userNotFound(next)
+  } else{
+    const user = ModelA.findById(req.session.userId)
+    res.json(user)
+  }
+})
+
+router.post('/signup', async(req, res, next) => {
+   try{
+     await ModelA.create(req.body)
+  } catch (error){
+    next(error)
+  }
+})
+
+router.put('/login', async (req, res, next) => {
+  try{
+    const user = await ModelA.findOne({
+      where: {
+        username: req.body.username
+      }
+    })
+
+    if (!user){
+      res.status(401).send("Not a valid username")
+    } else if(!user.correctPassword(req.body.password)){
+      res.status(401).send("Incorrect Password")
+    } else{
+      req.login(user, error => {
+        if(error){
+          next(error)
+        } else{
+          res.json(user)
+        }
+      })
+    }
+  } catch (error){
+    next(error)
+  }
+})
+
+router.delete('/logout', (req,res,next)=>{
+  req.logout()
+  req.session.destroy()
+  res.status(204).end()
+})
+
+
+
 module.exports = router
